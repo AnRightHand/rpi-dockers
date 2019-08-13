060bd9feefd4ca2352bdd55a1d815ae657db3a60
AYM <anyaming0@gmail.com>
2019-08-13 16:53:56 +0800

    x86-64 run by bash

--- x86_64/get.sh
+++ x86_64/get.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-wget https://github.com/AnRightHand/rpi-dockers/raw/master/aria2/aria2.conf
+wget https://github.com/AnRightHand/rpi-dockers/raw/master/x86_64/docker-compose.yml
+wget https://github.com/AnRightHand/rpi-dockers/raw/master/x86_64/Dockerfile-aria2
+wget https://github.com/AnRightHand/rpi-dockers/raw/master/x86_64/Dockerfile-caddy
+wget https://github.com/AnRightHand/rpi-dockers/raw/master/x86_64/Dockerfile-baiduPCS-Web
+wget https://github.com/AnRightHand/rpi-dockers/raw/master/x86_64/Dockerfile-minidlna
+wget https://github.com/AnRightHand/rpi-dockers/raw/master/x86_64/README.md
