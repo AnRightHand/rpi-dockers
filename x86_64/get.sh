59a1746d823e14b50801714ee4dc323869548a1e
AYM <anyaming0@gmail.com>
2019-08-13 17:16:16 +0800

    fixed default config used error

--- x86_64/get.sh
+++ x86_64/get.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-wget https://github.com/AnRightHand/rpi-dockers/raw/master/x86_64/docker-compose.yml
-wget https://github.com/AnRightHand/rpi-dockers/raw/master/x86_64/Dockerfile-aria2
-wget https://github.com/AnRightHand/rpi-dockers/raw/master/x86_64/Dockerfile-caddy
-wget https://github.com/AnRightHand/rpi-dockers/raw/master/x86_64/Dockerfile-baiduPCS-Web
-wget https://github.com/AnRightHand/rpi-dockers/raw/master/x86_64/Dockerfile-minidlna
-wget https://github.com/AnRightHand/rpi-dockers/raw/master/x86_64/README.md
+wget https://raw.githubusercontent.com/AnRightHand/rpi-dockers/master/x86_64/docker-compose.yml
+wget https://raw.githubusercontent.com/AnRightHand/rpi-dockers/master/x86_64/Dockerfile-aria2
+wget https://raw.githubusercontent.com/AnRightHand/rpi-dockers/master/x86_64/Dockerfile-caddy
+wget https://raw.githubusercontent.com/AnRightHand/rpi-dockers/master/x86_64/Dockerfile-baiduPCS-Web
+wget https://raw.githubusercontent.com/AnRightHand/rpi-dockers/master/x86_64/Dockerfile-minidlna
+wget https://raw.githubusercontent.com/AnRightHand/rpi-dockers/master/x86_64/README.md
