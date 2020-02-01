5526a80dcf933437657df7b7cb22ce10aa0f0351
AYMTools <anyaming0@gmail.com>
2020-02-01 13:42:49 +0800

    Update run.sh

--- caddy/run.sh
+++ caddy/run.sh
@@ -2,7 +2,7 @@
 
 mkdir -p /app
 if [[ ! -f "/config/Caddyfile" ]]; then
-  curl -f -sSL -o /app/Caddyfile https://raw.githubusercontent.com/AnRightHand/rpi-dockers/master/caddy/Caddyfile || true
+  curl -f -sSL -o /app/Caddyfile https://raw.githubusercontent.com/aymtools/rpi-dockers/master/caddy/Caddyfile || true
   mv -n /app/Caddyfile /config/Caddyfile
 fi
 
