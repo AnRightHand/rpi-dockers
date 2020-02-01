2bfadbd2131e5fa931cbc6666bc5ddf8f2b39245
AYMTools <anyaming0@gmail.com>
2020-02-01 13:45:34 +0800

    Update run.sh

--- minidlna/run.sh
+++ minidlna/run.sh
@@ -2,7 +2,7 @@
 
 mkdir -p /app 
 if [[ ! -f "/config/minidlna.conf" ]]; then
-  curl -sSL -o /app/minidlna.conf https://raw.githubusercontent.com/AnRightHand/rpi-dockers/master/minidlna/minidlna.conf || true
+  curl -sSL -o /app/minidlna.conf https://raw.githubusercontent.com/aymtools/rpi-dockers/master/minidlna/minidlna.conf || true
   mv -n /app/minidlna.conf /config/minidlna.conf
 fi
   
