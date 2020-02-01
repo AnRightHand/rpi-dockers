2b666043d3103f6203095decb133848821f37ead
AYMTools <anyaming0@gmail.com>
2020-02-01 13:43:15 +0800

    Update run.sh

--- filebrowser/run.sh
+++ filebrowser/run.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 
 if [[ ! -f "/config/filebrowser.json" ]]; then
-  curl -f -sSL -o /app/filebrowser.json https://raw.githubusercontent.com/AnRightHand/rpi-dockers/master/filebrowser/filebrowser.json || true
+  curl -f -sSL -o /app/filebrowser.json https://raw.githubusercontent.com/aymtools/rpi-dockers/master/filebrowser/filebrowser.json || true
   mv -n /app/filebrowser.json /config/filebrowser.json
 fi
 if [[ ! -f "/config/database.db" ]]; then
