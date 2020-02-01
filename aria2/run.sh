d541bfe84b0e2db64ba8bb3f81b798f1af9324c3
AYMTools <anyaming0@gmail.com>
2020-02-01 13:39:58 +0800

    Update run.sh

--- aria2/run.sh
+++ aria2/run.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 if [[ ! -f "/config/aria2.conf" ]]; then
-  curl -f -sSL -o /app/aria2.conf https://raw.githubusercontent.com/AnRightHand/rpi-dockers/master/aria2/aria2.conf || true
+  curl -f -sSL -o /app/aria2.conf https://raw.githubusercontent.com/aymtools/rpi-dockers/master/aria2/aria2.conf || true
   mv -n /app/aria2.conf /config/aria2.conf
 fi
 if [[ ! -f "/config/aria2.session" ]]; then
