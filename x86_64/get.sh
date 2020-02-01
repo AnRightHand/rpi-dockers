88f824d2d90d7e79575c12afea3c807c60b76a7c
AYMTools <anyaming0@gmail.com>
2020-02-01 13:50:46 +0800

    Update get.sh

--- x86_64/get.sh
+++ x86_64/get.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-curl -O https://raw.githubusercontent.com/AnRightHand/rpi-dockers/master/x86_64/docker-compose.yml
-curl -O https://raw.githubusercontent.com/AnRightHand/rpi-dockers/master/x86_64/Dockerfile-aria2
-curl -O https://raw.githubusercontent.com/AnRightHand/rpi-dockers/master/x86_64/Dockerfile-caddy
-curl -O https://raw.githubusercontent.com/AnRightHand/rpi-dockers/master/x86_64/Dockerfile-baiduPCS-Web
-curl -O https://raw.githubusercontent.com/AnRightHand/rpi-dockers/master/x86_64/Dockerfile-minidlna
-curl -O https://raw.githubusercontent.com/AnRightHand/rpi-dockers/master/x86_64/Dockerfile-filebrowser
-curl -O https://raw.githubusercontent.com/AnRightHand/rpi-dockers/master/x86_64/Dockerfile-syncthing
-curl -O https://raw.githubusercontent.com/AnRightHand/rpi-dockers/master/x86_64/Dockerfile-v2
-curl -O https://raw.githubusercontent.com/AnRightHand/rpi-dockers/master/x86_64/README.md
+curl -O https://raw.githubusercontent.com/aymtools/rpi-dockers/master/x86_64/docker-compose.yml
+curl -O https://raw.githubusercontent.com/aymtools/rpi-dockers/master/x86_64/Dockerfile-aria2
+curl -O https://raw.githubusercontent.com/aymtools/rpi-dockers/master/x86_64/Dockerfile-caddy
+curl -O https://raw.githubusercontent.com/aymtools/rpi-dockers/master/x86_64/Dockerfile-baiduPCS-Web
+curl -O https://raw.githubusercontent.com/aymtools/rpi-dockers/master/x86_64/Dockerfile-minidlna
+curl -O https://raw.githubusercontent.com/aymtools/rpi-dockers/master/x86_64/Dockerfile-filebrowser
+curl -O https://raw.githubusercontent.com/aymtools/rpi-dockers/master/x86_64/Dockerfile-syncthing
+curl -O https://raw.githubusercontent.com/aymtools/rpi-dockers/master/x86_64/Dockerfile-v2
+curl -O https://raw.githubusercontent.com/aymtools/rpi-dockers/master/x86_64/README.md
